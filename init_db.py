808c4e9645b421d92855d84ca65020b98b552397
Kowshik0910 <kowshik.m@arkatiss.com>
2025-12-16 18:22:44 +0530

    init_db file added

---
+++ init_db.py
@@ -0,0 +1,30 @@
+from common.config import get_connection
+
+conn = get_connection("EXPT")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS et_users (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(100) UNIQUE NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS expense_logs (
+    expense_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES et_users(user_id) ON DELETE CASCADE,
+    category VARCHAR(100),
+    description TEXT,
+    amount NUMERIC(10,2),
+    log_creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print("✅ Database tables created successfully")
